c1faa5e3a5989ad0dffc4434aa1582107f164962
MatiRosas31 <165863634+MatiRosas31@users.noreply.github.com>
2024-10-09 23:28:09 +0000

    [Tarea completada] -En primer lugar se creo la estructura basica del todolist. Se deja fija el primer input. Pero dentro de las propiedades de ese input se agrega un propiedad value con el valor de la variable del hook useState -todo- Tambien se agrega un atributo onChange con una function flecha con un argumento "event" o "e" y llamando la function setTodo con los parametros: e.target. value -Y luego se agrego un atributo onKeyDown que llama a la function "addTodo" -Se agregaron dos functions addTodo y removeTodo -addTodo evalua si el evento fue una key press igual a "Enter" y la variable todo no esta vacia De ser asi, se utilia function setTodo con un argumento que copia el array arrtodos y adiciona la variable todo que almacena lo que haya escrito el user - La function removeTodo tambien hace una copia y utiliza el metodo esplice para remover el elemen to. Se pone como parametro index para determinar cual elemento del array ha de ser eliminado Y luego se llama a setTodos con la copia del array -y sus respectivas modificaciones-  # Please enter the commit message for your changes. Lines starting

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+const [todo,setTodo] = useState("")
+const [arrtodos, setTodos] = useState(
+	["Make the bed","Wash my hands", "Eat", "Walk the dog"]
+)
+const addTodo = (e) => {
+    if (e.key === "Enter" && todo !== "") {
+		//Se hace una copia del array arrtodos y se le agrega el nuevo item (todo)
+      setTodos([...arrtodos, todo]);
+	  //Para limpiar lo que haya escrito el user
+      setTodo("");
+    }
+  };
+
+  const removeTodo = (index) => {
+	const todosCopy = [...arrtodos];
+	todosCopy.splice(index, 1);
+	setTodos(todosCopy)
+  }
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div className="text-center container mt-5">
+			<h1 className="title">My ToDo List</h1>
+			<div className="card">
+				<ul className="list-group list-group-flush">
+					<li className="list-group-item"> 
+						<input className="form-control" placeholder="What needs to be done?" value={todo} 
+						onChange={(e) => setTodo(e.target.value)}
+						onKeyPress={addTodo}
+						/>
+					</li>
+					{arrtodos.map((item,index) => (
+						<li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+						{item}
+						<button className="btn btn-danger me-2" onClick={()=> removeTodo(index)}>X</button>
+						</li>
+					))}
+				</ul>
+			</div>
+			<span>{arrtodos.length} items left</span>
 		</div>
+		</>
 	);
 };
 
