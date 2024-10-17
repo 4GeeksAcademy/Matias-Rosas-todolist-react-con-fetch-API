4c09e5663f3e500d9220547a2f80c9a6c7674ea8
MatiRosas31 <165863634+MatiRosas31@users.noreply.github.com>
2024-10-17 01:08:12 +0000

    falta arreglar el problema con la function changeCheckboxStatus

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,124 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 
 //create your first component
 const Home = () => {
 const [todo,setTodo] = useState("")
-const [arrtodos, setTodos] = useState(
-	["Make the bed","Wash my hands", "Eat", "Walk the dog"]
-)
-const addTodo = (e) => {
-    if (e.key === "Enter" && todo !== "") {
-		//Se hace una copia del array arrtodos y se le agrega el nuevo item (todo)
-      setTodos([...arrtodos, todo]);
-	  //Para limpiar lo que haya escrito el user
-      setTodo("");
-    }
-  };
+const [todos, setTodos] = useState([])
+const [donestate, setDonestate] = useState(false)
 
-  const removeTodo = (index) => {
-	const todosCopy = [...arrtodos];
-	todosCopy.splice(index, 1);
-	setTodos(todosCopy)
-  }
+function getTodos () {
+	fetch("https://playground.4geeks.com/todo/users/MatiRosas31")
+	.then((resp)=>{
+		console.log("Archivo como json: ",resp.status)
+		return resp.json()
+	})
+	.then((data)=>{
+		console.log("Archivo como objeto de JS: ", data)
+		setTodos(data.todos)
+	})
+	.catch((error) => {return error})
+}
+
+useEffect(()=>{
+	getTodos()
+},[]);
+
+function addTodo (todoLabel) {
+	if (todoLabel !== "") {
+		let newTask = {
+			label: todoLabel,
+			is_done: false
+		}
+		fetch ("https://playground.4geeks.com/todo/todos/MatiRosas31", {
+			method: "POST",
+			body: JSON.stringify(newTask),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+		.then(resp => {
+			console.log(resp.ok); 
+			console.log(resp.status); 
+			console.log(resp.text()); 
+			return resp.json(); 
+		})
+		.then(data => {
+			console.log(data);
+	
+		})
+		.catch(error => {
+			console.error(error);
+	
+		});
+	}
+}
+function removeTodo(index) {
+	todos.map((value)=> {
+		if (value.id === index) {
+			fetch(`https://playground.4geeks.com/todo/todos/${index}`, {
+
+				method: "DELETE",
+		  
+				body: JSON.stringify(todos),
+		  
+				headers: {
+		  
+				  "Content-Type": "application/json"
+		  
+				}
+				
+			  })
+			  .then(resp => {
+				if (resp.status === 204) {
+					getTodos()
+				}
+				console.log(resp.ok); 
+				console.log(resp.status); 
+				console.log(resp.text()); 
+			})
+			.catch((error)=>{
+				console.log(error)
+			})
+		}
+	})
+}
+
+function changeCheckboxStatus (e, todoId) {
+	const isChecked = e.target.checked;
+	let updatedstatus = {
+		is_done: isChecked
+	};
+todos.map((value)=> {
+	if (value.id === todoId) {
+		fetch(`https://playground.4geeks.com/todo/todos/${todoId}`, {
+			method: "PUT",
+			body: JSON.stringify(updatedstatus),
+			headers: {
+				"Content-Type": "application/json"
+			}
+			.then(resp => {
+				if (resp.status === 204) {
+					getTodos()
+				}
+			})
+			.catch(error => {
+				console.log(error);
+			})
+	
+		})
+	}
+})
+	
+}
+
+const handleKeyDown = (e, todo) => {
+	if (e.key === 'Enter') { 
+	  e.preventDefault();
+	  addTodo(todo); 
+	}
+  };
+  
 	return (
 		<>
 		<div className="text-center container mt-5">
@@ -30,26 +128,29 @@ const addTodo = (e) => {
 			<div className="card pb-1 me-1">
 				<ul className="list-group list-group-flush">
 					<li className="list-group-item"> 
-						<input className="form-control" placeholder="What needs to be done?" value={todo} 
-						onChange={(e) => setTodo(e.target.value)}
-						onKeyPress={addTodo}
+						<input className="form-control" placeholder="What needs to be done?" value={todo} onChange={(e) => setTodo(e.target.value)} onKeyDown={(e) => handleKeyDown(e, todo)} 
 						/>
 					</li>
-					{arrtodos.map((item,index) => (
-						<li key={index} className="list-group-item d-flex justify-content-between align-items-center">
-						{item}
-						<button className="boton boton-danger me-2" onClick={()=> removeTodo(index)}>X</button>
+					{todos.map((item,index) => (
+						<li key={index} className="list-group-item d-flex justify-content-between align-items-start">
+						<div className="d-flex align-items-center">
+							<div className="form-check">
+  								<input className="form-check-input" type="checkbox"  checked={item.is_done}  onChange={(e) => changeCheckboxStatus(e, item.id)} />
+  									<label className="form-check-label" htmlFor="flexCheckDefault">
+  									</label>
+								</div>
+								<span className="ms-2">{item.label}</span>
+							</div>
+						<button onClick={()=> removeTodo(item.id) }  className="boton boton-danger me-2" >X</button>
 						</li>
 					))}
 				</ul>
 				<div className="row text-start">
-				<span className="ms-1 text-secondary">{arrtodos.length < 1 ? "No hay tareas, añadir tareas" : arrtodos.length + " items" +  " left"}</span>
+				<span className="ms-1 text-secondary">{todos.length < 1 ? "No hay tareas, añadir tareas" : todos.length + " items" +  " left"}</span>
 				</div>
 			</div>
 			</div>				
 			</div>
-			
-			
 		</div>
 		</>
 	);
