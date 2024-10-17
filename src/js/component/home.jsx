f77427405dbcda6592ce2b09e8913d54c1b0be1e
MatiRosas31 <165863634+MatiRosas31@users.noreply.github.com>
2024-10-17 21:26:22 +0000

    [Bug actualizar la pagina para ver resultados] ~Se arreglo el bug, ahora no hay necesidad de refrescar la pagina para ver los cambios (eliminar ,agregar o checkear tareas) -El problema es que no se estaban volviendo a renderizar los componentes - No se estaba volviendo a actualizar el estado del arrray "todos"  despues de hacer las peticio- nes a la API. - En la function addTodo se invoca la function del useState "setTodos" para cambiar el estado de "todos" y se crea una peque;a function que copia el arreglo "todos" pero agrega la nueva task con el valor de "label" actualizado. -En la function de removeTodo se agrego una función filter para crear un nuevo array que contiene solo los elementos cuyo id no coincide con todoId (es decir, la tarea a eliminar). -Luego en la function "changeCheckboxStatus" se creo un if para evaluar si el id que le pasan coincide con alguno del array. De coincidir se actualiza la tarea usando setTodos con parametro "updatedTodos"

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,161 +1,145 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
-
-//create your first component
 const Home = () => {
-const [todo,setTodo] = useState("")
-const [todos, setTodos] = useState([])
-const [donestate, setDonestate] = useState(false)
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
 
-function getTodos () {
-	fetch("https://playground.4geeks.com/todo/users/MatiRosas31")
-	.then((resp)=>{
-		console.log("Archivo como json: ",resp.status)
-		return resp.json()
-	})
-	.then((data)=>{
-		console.log("Archivo como objeto de JS: ", data)
-		setTodos(data.todos)
-	})
-	.catch((error) => {return error})
-}
+  useEffect(() => {
+    getTodos(); //Con el useEffect el codigo se ejecuta solo una vez y asi evitar que cree infinitos pedidos a la API
+  }, []);
 
-useEffect(()=>{
-	getTodos()
-},[]);
-
-function addTodo (todoLabel) {
-	if (todoLabel !== "") {
-		let newTask = {
-			label: todoLabel,
-			is_done: false
-		}
-		fetch ("https://playground.4geeks.com/todo/todos/MatiRosas31", {
-			method: "POST",
-			body: JSON.stringify(newTask),
-			headers: {
-				"Content-Type": "application/json"
-			}
-		})
-		.then(resp => {
-			console.log(resp.ok); 
-			console.log(resp.status); 
-			console.log(resp.text()); 
-			return resp.json(); 
-		})
-		.then(data => {
-			console.log(data);
-	
-		})
-		.catch(error => {
-			console.error(error);
-	
-		});
-	}
-}
-function removeTodo(index) {
-	todos.map((value)=> {
-		if (value.id === index) {
-			fetch(`https://playground.4geeks.com/todo/todos/${index}`, {
-
-				method: "DELETE",
-		  
-				body: JSON.stringify(todos),
-		  
-				headers: {
-		  
-				  "Content-Type": "application/json"
-		  
-				}
-				
-			  })
-			  .then(resp => {
-				if (resp.status === 204) {
-					getTodos()
-				}
-				console.log(resp.ok); 
-				console.log(resp.status); 
-				console.log(resp.text()); 
-			})
-			.catch((error)=>{
-				console.log(error)
-			})
-		}
-	})
-}
-
-function changeCheckboxStatus(e, todoId) {
-	const isChecked = e.target.checked;
-	let updatedstatus = {
-	  is_done: isChecked
-	};
-  
-	// Reemplazar 'index' por 'todoId'
-	fetch(`https://playground.4geeks.com/todo/todos/${todoId}`, {
-	  method: "PUT",
-	  body: JSON.stringify(updatedstatus),
-	  headers: {
-		"Content-Type": "application/json"
-	  }
-	})
-	.then(resp => {
-	  console.log(resp.ok); 
-	  console.log(resp.status); 
-	  console.log(resp.text()); 
-	  return resp.json();
-	})
-	.then(data => {
-	  console.log("Update response:", data);
-	  getTodos(); // Actualiza la lista de tareas después de cambiar el estado
-	})
-	.catch(error => {
-	  console.log(error);
-	});
+  function getTodos() {
+    fetch("https://playground.4geeks.com/todo/users/MatiRosas31")
+      .then((resp) => resp.json())
+      .then((data) => setTodos(data.todos))
+      .catch((error) => console.error(error));
   }
-  
 
-const handleKeyDown = (e, todo) => {
-	if (e.key === 'Enter') { 
-	  e.preventDefault();
-	  addTodo(todo); 
-	}
+  function addTodo(todoLabel) {
+    if (todoLabel !== "") {
+      let newTask = {
+        label: todoLabel,
+        is_done: false,
+      };
+      fetch("https://playground.4geeks.com/todo/todos/MatiRosas31", {
+        method: "POST",
+        body: JSON.stringify(newTask),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((resp) => resp.json())
+        .then(() => {
+          setTodos((prevTodos) => [...prevTodos, newTask]); //copia del array pero se agrega la nueva task/to-do
+        })
+        .catch((error) => console.error(error));
+    }
+  }
+
+  function removeTodo(todoId) {
+    fetch(`https://playground.4geeks.com/todo/todos/${todoId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => {
+        if (resp.status === 204) {
+          setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== todoId));//La función filter se usa para crear un nuevo array que contiene solo los elementos cuyo id no coincide con todoId (es decir, la tarea a eliminar).
+        }
+      })
+      .catch((error) => console.error(error));
+  }
+
+  function changeCheckboxStatus(e, todoId) {
+    const isChecked = e.target.checked;
+
+    const updatedTodos = todos.map((todo) => {
+		if (todo.id === todoId) {
+		  return { ...todo, is_done: isChecked }; // Si coincide el id, actualiza la tarea
+		} else {
+		  return todo; // Si no coincide, devuelve la tarea original sin cambios
+		}
+	  });
+	  
+	  setTodos(updatedTodos); // Actualiza el estado con el array modificado
+
+    fetch(`https://playground.4geeks.com/todo/todos/${todoId}`, {
+      method: "PUT",
+      body: JSON.stringify({ is_done: isChecked }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log("Update response:", data);
+      })
+      .catch((error) => console.error(error));
+  }
+
+  const handleKeyDown = (e, todo) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      addTodo(todo);
+    }
   };
-  
-	return (
-		<>
-		<div className="text-center container mt-5">
-			<h1 className="mytitle text-black-50">todos</h1>
-			<div className="card shadow pb-2 pe-2 rounded"> 
-			<div className="card pb-2 pe-1">
-			<div className="card pb-1 me-1">
-				<ul className="list-group list-group-flush">
-					<li className="list-group-item"> 
-						<input className="form-control" placeholder="What needs to be done?" value={todo} onChange={(e) => setTodo(e.target.value)} onKeyDown={(e) => handleKeyDown(e, todo)} 
-						/>
-					</li>
-					{todos.map((item,index) => (
-						<li key={index} className="list-group-item d-flex justify-content-between align-items-start">
-						<div className="d-flex align-items-center">
-							<div className="form-check">
-  								<input className="form-check-input" type="checkbox"  checked={item.is_done}  onChange={(e) => changeCheckboxStatus(e, item.id)} />
-  									<label className="form-check-label" htmlFor="flexCheckDefault">
-  									</label>
-								</div>
-								<span className="ms-2">{item.label}</span>
-							</div>
-						<button onClick={()=> removeTodo(item.id) }  className="boton boton-danger me-2" >X</button>
-						</li>
-					))}
-				</ul>
-				<div className="row text-start">
-				<span className="ms-1 text-secondary">{todos.length < 1 ? "No hay tareas, añadir tareas" : todos.length + " items" +  " left"}</span>
-				</div>
-			</div>
-			</div>				
-			</div>
-		</div>
-		</>
-	);
+
+  return (
+    <>
+      <div className="text-center container mt-5">
+        <h1 className="mytitle text-black-50">todos</h1>
+        <div className="card shadow pb-2 pe-2 rounded">
+          <div className="card pb-2 pe-1">
+            <div className="card pb-1 me-1">
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                  <input
+                    className="form-control"
+                    placeholder="What needs to be done?"
+                    value={todo}
+                    onChange={(e) => setTodo(e.target.value)}
+                    onKeyDown={(e) => handleKeyDown(e, todo)}
+                  />
+                </li>
+                {todos.map((item, index) => (
+                  <li
+                    key={index}
+                    className="list-group-item d-flex justify-content-between align-items-start"
+                  >
+                    <div className="d-flex align-items-center">
+                      <div className="form-check">
+                        <input
+                          className="form-check-input"
+                          type="checkbox"
+                          checked={item.is_done}
+                          onChange={(e) => changeCheckboxStatus(e, item.id)}
+                        />
+                      </div>
+                      <span className="ms-2">{item.label}</span>
+                    </div>
+                    <button
+                      onClick={() => removeTodo(item.id)}
+                      className="boton boton-danger me-2"
+                    >
+                      X
+                    </button>
+                  </li>
+                ))}
+              </ul>
+              <div className="row text-start">
+                <span className="ms-1 text-secondary">
+                  {todos.length < 1
+                    ? "No hay tareas, añadir tareas"
+                    : todos.length + " items left"}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
